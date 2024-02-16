641586e1bfc23e425113eb8332844d8e03fbe826
luv <116978932+methdealer52@users.noreply.github.com>
2024-02-16 19:17:28 +0100

    did some hud stuff

--- src/main/java/sn0w/OyVey.java
+++ src/main/java/sn0w/OyVey.java
@@ -8,12 +8,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.Display;
 
-@Mod(modid = "oyvey", name = "OyVey", version = "0.0.3")
+@Mod(modid = "sn0w", name = "Sn0w", version = "1.0")
 public class OyVey {
-    public static final String MODID = "oyvey";
-    public static final String MODNAME = "OyVey";
-    public static final String MODVER = "0.0.3";
-    public static final Logger LOGGER = LogManager.getLogger("OyVey");
+    public static final String MODID = "sn0w";
+    public static final String MODNAME = "Sn0w";
+    public static final String MODVER = "1,0";
+    public static final Logger LOGGER = LogManager.getLogger("Sn0w");
     public static CommandManager commandManager;
     public static FriendManager friendManager;
     public static ModuleManager moduleManager;
@@ -40,7 +40,7 @@ public class OyVey {
     }
 
     public static void load() {
-        LOGGER.info("\n\nLoading OyVey by Alpha432");
+        LOGGER.info("\n\nLoading Sn0w by nw0p");
         unloaded = false;
         if (reloadManager != null) {
             reloadManager.unload();
@@ -70,11 +70,11 @@ public class OyVey {
         LOGGER.info("EventManager loaded.");
         textManager.init(true);
         moduleManager.onLoad();
-        LOGGER.info("OyVey successfully loaded!\n");
+        LOGGER.info("Sn0w successfully loaded!\n");
     }
 
     public static void unload(boolean unload) {
-        LOGGER.info("\n\nUnloading OyVey by Alpha432");
+        LOGGER.info("\n\nUnloading Sn0w by Alpha432");
         if (unload) {
             reloadManager = new ReloadManager();
             reloadManager.init(commandManager != null ? commandManager.getPrefix() : ".");
@@ -95,7 +95,7 @@ public class OyVey {
         inventoryManager = null;
         moduleManager = null;
         textManager = null;
-        LOGGER.info("OyVey unloaded!\n");
+        LOGGER.info("Sn0w unloaded!\n");
     }
 
     public static void reload() {
@@ -120,7 +120,7 @@ public class OyVey {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        Display.setTitle("OyVey v0.0.3");
+        Display.setTitle("Sn0w 1.0 | Minecraft 1.12.2");
         OyVey.load();
     }
 }
