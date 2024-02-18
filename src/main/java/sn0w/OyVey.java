d6a0805065b27edeaaddefa91d79481fa368d2dd
luv <116978932+methdealer52@users.noreply.github.com>
2024-02-18 04:36:42 +0100

    Update OyVey.java

--- src/main/java/sn0w/OyVey.java
+++ src/main/java/sn0w/OyVey.java
@@ -13,7 +13,7 @@ import sn0w.manager.*;
 public class OyVey {
     public static final String MODID = "sn0w";
     public static final String MODNAME = "Sn0w";
-    public static final String PREFIX = ChatFormatting.AQUA + "\u2744";
+    public static final String PREFIX = ChatFormatting.BLUE + "[" + "\u2744" + "]";
     public static final Logger LOGGER = LogManager.getLogger("Sn0w");
     public static CommandManager commandManager;
     public static FriendManager friendManager;
